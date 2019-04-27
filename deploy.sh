7f93e57cfc63d475415e5e069357c4a73f327110
Fazitronics <fazitronics@gmail.com>
2019-04-27 17:11:00 +1000

    Update script

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,43 @@
-#!/bin/bash
+#!/bin/sh
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
+# Commit to master
+git push origin master
 
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-git commit -m "$msg"
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+cp CNAME public/
+
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
+git push origin gh-pages
+
+git worktree remove public
 
 # Come Back up to the Project Root
 cd ..
-
-# Commit to master
-git add .
-git commit -m "$msg"
-git push origin master
